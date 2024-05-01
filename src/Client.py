bcd6411cffba0c725eea835d338bf3321f615309
Lucas-Pardina-UNC <lucas.pardina@mi.unc.edu.ar>
2024-05-01 13:52:36 -0300

    Agregar endpoint de login y lógica de autenticación requerida para utilizar el endpoint que recibe y ejecuta comandos - Añadir .gitignore

--- src/Client.py
+++ src/Client.py
@@ -1,28 +1,61 @@
 import requests
-from flask import jsonify 
+from flask import jsonify
 
-def send_command(command):
+def login():
+    try:
+        while True:
+            username = input("Ingrese su email o nombre de usuario: ")
+            password = input("Ingrese su contraseña: ")
+            data = {'username': username, 'password': password}
+            # Enviar credenciales al servidor
+            response = requests.post('http://127.0.0.1:5000/login', json=data) # Bloqueante => Epera por la respuesta del servidor
+
+            # Verificar si la autenticación fue exiosa (status code 200)
+            if response.status_code == 200:
+                access_token = response.json().get('access_token')
+                return access_token
+            # Verificar si las credenciales ingresadas eran válidas (status code 401)
+            elif response.status_code == 401:
+                print("Invalid username or password. Please try again.")
+            # Manejar otros posibles HTTP status codes
+            else:
+                print(f"Falla al autenticarse. Status code: {response.status_code}")
+    except Exception as e:
+        print("Ha ocurrido un error:", type(e).__name__, "-", e)
+        return None
+
+def send_command(access_token):
     # URL del servidor donde se enviará la solicitud POST
     url = 'http://127.0.0.1:5000/execute_command'
     #url = 'https://108f-190-30-79-98.ngrok-free.app/execute_command'
-    # Datos a enviar en la solicitud POST (comando)
-    data = {'command': command}
-    try:
-        # Enviar la solicitud POST al servidor
-        response = requests.post(url, json=data) # Bloqueante => Epera por la respuesta del servidor
-        # Devolver el contenido JSON de la respuesta
-        return response.json()
-    except Exception as e:
-        print("Ha ocurrido un error:", type(e).__name__, "-", e)
-        return jsonify({'error': str(e)}), 500
     
-while True:
-    command = input("Ingrese un comando ('exit' para salir): ")
-    if command.lower() == 'exit':  # Verificar si el usuario quiere salir
-        break
-    result = send_command(command)
-    print("Resultado:")
-    print("Comando ejecutado:", result['command'])
-    print("STDOUT:", result['stdout'])
-    print("STDERR:", result['stderr'])
-    print("Código de retorno:", result['returncode'])
+    while True:
+        command = input("Ingrese un comando ('exit' para salir): ")
+        if command.lower() == 'exit':  # Verificar si el usuario quiere salir
+            break
+        
+        headers = {'Authorization': f'Bearer {access_token}'}
+        # Datos a enviar en la solicitud POST (comando)
+        data = {'command': command} # En el campo command del json, incluir el comando
+        try:
+            # Enviar la solicitud POST al servidor incluyendo el comando en 'json' y las credenciales de autenticación en 'headers'
+            response = requests.post(url, headers=headers, json=data) # Bloqueante => Epera por la respuesta del servidor
+            # Si el http request fue exitoso, imprimir el contenido JSON de la respuesta
+            if response.status_code == 200:
+                result = response.json()
+                print("Resultado:")
+                print("Comando ejecutado:", result['command'])
+                print("STDOUT:", result['stdout'])
+                print("STDERR:", result['stderr'])
+                print("Código de retorno:", result['returncode'])
+            else:
+                print("No se pudo ejecutar el comando. Status code:", response.status_code)
+        except Exception as e:
+            print("Ha ocurrido un error:", type(e).__name__, "-", e)
+            return jsonify({'error': str(e)}), 500
+
+if __name__ == "__main__":
+    
+    access_token = login()
+    if access_token:
+        send_command(access_token)
--- src/Server.py
+++ src/Server.py
@@ -1,14 +1,64 @@
 from flask import Flask, request, jsonify # pip install Flask
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity # Framework de seguridad
+import subprocess                         # El módulo subprocess, que permite ejecutar comandos del sistema operativo desde Python
+import secrets                            # Generar JWT_SECRET_KEY aleatoria para mantener la confidencialidad
+from flask_sqlalchemy import SQLAlchemy   # Base de datos para registrar clientes
+from passlib.hash import pbkdf2_sha256    # Para password hashing
 
-import subprocess   #El módulo subprocess, que permite ejecutar comandos del sistema operativo desde Python
+# Generar una clave secreta aleatoria segura
+secret_key = secrets.token_hex(32)
 
-app = Flask(__name__) # Creación de una instancia de la clase Flask
+# Creación de una instancia de la clase Flask
+app = Flask(__name__) 
+app.config['JWT_SECRET_KEY'] = secret_key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # SQLite database URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Definición de un endpoint '/execute_command' que recibe solicitudes POST
+# Inicializar Flask-JWT-Extended
+jwt = JWTManager(app)
+
+# Inicializar base de datos
+db = SQLAlchemy(app)
+
+# Definir modelo para la base de datos
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+# Crear usuarios de prueba (esto va a ser reemplazado por una base de datos)
+users = {
+    'user1': 'password1',
+    'user2': 'password2'
+}
+
+# Definir un endpoint para login
+@app.route('/login', methods=['POST'])
+def login():
+    try:    
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        # Buscar coincidencias con el usuario en la base de datos
+        user = User.query.filter_by(username=username).first()
+
+        # Existen coincidencias => user <User object> ; No existen coincidencias => user = None
+        if user and pbkdf2_sha256.verify(password, user.password):  # pbkdf2_sha256.verify realiza la comparación con la contraseña encriptada
+            # Crear un JWT token
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token), 200
+        else:
+            return jsonify({'message': 'Usuario o contraseña incorrectos'}), 401
+    except Exception as e:
+        print("Ha ocurrido un error:", type(e).__name__, "-", e)
+        return jsonify({'error': str(e)}), 500
+
+# Definir que recibe solicitudes POST (espera comandos de red en formato json para ejecutar)
 @app.route('/execute_command', methods=['POST'])
+@jwt_required()  # Requiere autenticación
 def execute_command():
     try:
-        # Obtención de los datos enviados en la solicitud POST en formato JSON
+        # Acceder de los datos del HTTP request (enviados en la solicitud POST) en formato JSON
         data = request.get_json()
         # Extracción del comando enviado en los datos recibidos
         command = data['command']
@@ -21,6 +71,7 @@ def execute_command():
     except Exception as e:
         print("Ha ocurrido un error:", type(e).__name__, "-", e)
         return jsonify({'error': str(e)}), 500
+
 # Función para ejecutar comandos y devolver el resultado en formato JSON
 def run_command(command):
     try:
@@ -39,5 +90,27 @@ def run_command(command):
 
 # Condición para ejecutar el servidor si este script es el punto de entrada principal
 if __name__ == '__main__':
+    with app.app_context():
+        # Crear las tablas de la base de datos (si aún no existen)
+        db.create_all()
+
+        # -------------------- Reemplazar por endpoint para registro de usuarios ----------------------
+        # Función para agregar usuarios
+        def add_user(username, password):
+            # Check if the username already exists
+            existing_user = User.query.filter_by(username=username).first()
+            if existing_user:
+                print("User with username '{}' already exists.".format(username))
+                return
+            else:
+                hashed_password = pbkdf2_sha256.hash(password)
+                new_user = User(username=username, password=hashed_password)
+                db.session.add(new_user)
+                db.session.commit()
+
+        # Agregar usuarios de prueba
+        add_user('user1', 'password1')
+        # ---------------------------------------------------------------------------------------------
+    
     # Iniciar el servidor Flask en la dirección IP 0.0.0.0 (accesible desde cualquier dirección IP) y en el puerto 5000
     app.run(host='0.0.0.0', port=5000)
